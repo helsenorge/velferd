51383b7835bd567760f0cf184659866c82ee00ea
Carlo Diaz <carlo.diaz@ehelse.no>
2016-05-03 13:59:30 +0000

    Initial front-end commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+      main: [
+        './src/main.js'
+      ]
+    },    
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'public'),
+        publicPath: '/public/'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, include: path.join(__dirname, 'src'), loader: 'babel'},
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
