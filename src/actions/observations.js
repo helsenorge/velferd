5379fa93bb99c4184b7029f2bd26eb7b2fefee22
Carlo Diaz <carlo.diaz@ehelse.no>
2016-05-10 12:48:45 +0000

    Using redux to manage state

---
+++ src/actions/observations.js
@@ -0,0 +1,30 @@
+export const REQUEST_OBSERVATIONS = 'REQUEST_OBSERVATIONS';
+export const RECEIVE_OBSERVATIONS = 'RECEIVE_OBSERVATIONS';
+
+function requestObservations(code, patientId) {
+  return {
+    type: REQUEST_OBSERVATIONS,
+    code,
+    patientId,
+  };
+}
+
+function receiveObservations(code, patientId, json) {
+  return {
+    type: RECEIVE_OBSERVATIONS,
+    code,
+    patientId,
+    data: json,
+    receivedAt: Date.now(),
+  };
+}
+
+export function fetchObservations(fhirUrl, code, patientId) {
+  return dispatch => {
+    dispatch(requestObservations(code, patientId));
+    const url = `${fhirUrl}/Observation?_count=500&_sort:desc=date&code=${code}&patient=${patientId}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(json => dispatch(receiveObservations(code, patientId, json)));
+  };
+}
---
+++ src/reducers/observations.js
@@ -0,0 +1,42 @@
+import { combineReducers } from 'redux';
+import {
+  REQUEST_OBSERVATIONS, RECEIVE_OBSERVATIONS,
+} from '../actions/observations';
+
+function observations(state = {
+  isFetching: false,
+  data: [],
+}, action) {
+  switch (action.type) {
+  case REQUEST_OBSERVATIONS:
+    return Object.assign({}, state, {
+      isFetching: true,
+    });
+  case RECEIVE_OBSERVATIONS:
+    return Object.assign({}, state, {
+      isFetching: false,
+      data: action.data,
+      lastUpdated: action.receivedAt,
+    });
+  default:
+    return state;
+  }
+}
+
+function observationsByCode(state = { }, action) {
+  switch (action.type) {
+  case REQUEST_OBSERVATIONS:
+  case RECEIVE_OBSERVATIONS:
+    return Object.assign({}, state, {
+      [action.code]: observations(state[action.code], action),
+    });
+  default:
+    return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  observationsByCode,
+});
+
+export default rootReducer;
