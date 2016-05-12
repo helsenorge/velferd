7f55e1e4dc3596674523f5a72430b7a2bdf02762
Carlo Diaz <carlo.diaz@ehelse.no>
2016-05-12 12:24:58 +0000

    Linting

--- src/actions/observations.js
+++ src/actions/observations.js
@@ -22,7 +22,8 @@ function receiveObservations(code, patientId, json) {
 export function fetchObservations(fhirUrl, code, patientId) {
   return dispatch => {
     dispatch(requestObservations(code, patientId));
-    const url = `${fhirUrl}/Observation?_count=500&_sort:desc=date&code=${code}&patient=${patientId}`;
+    const url =
+    `${fhirUrl}/Observation?_count=500&_sort:desc=date&code=${code}&patient=${patientId}`;
     return fetch(url)
       .then(response => response.json())
       .then(json => dispatch(receiveObservations(code, patientId, json)));
