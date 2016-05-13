88a0f8ece48b97bde3fe08b3f0679a8012bae4f9
Carlo Diaz <carlo.diaz@ehelse.no>
2016-05-13 10:38:32 +0000

    Output observations to console

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import './main.scss';
+import configureStore from './store/configureStore';
+import { fetchObservations } from './actions/observations';
+
+const store = configureStore();
+
+console.log(store.getState());
+store.subscribe(() =>
+  console.log(store.getState())
+);
+
+const fhirUrl = 'http://continua.cloudapp.net/baseDstu2';
+store.dispatch(fetchObservations(fhirUrl, '188736', '1-26'));
 
 ReactDOM.render(
   <App />,
