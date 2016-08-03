f6484452624fbced19f88c4d484eb388975da391
carlodiaz <carlo.diaz@ehelse.no>
2016-08-03 11:56:12 +0000

    Remove last value section

--- src/components/measurement/measurement.jsx
+++ src/components/measurement/measurement.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import Chart from '../chart/chart.jsx';
 import './measurement.scss';
 import ObservationCodes from '../../constants/observation-codes';
-import { formatDate, filterPoints } from '../date-helpers/date-helpers.js';
+import { filterPoints } from '../date-helpers/date-helpers.js';
 
 class Measurements extends Component {
 
@@ -90,26 +90,12 @@ class Measurements extends Component {
         />);
     }
 
-    let lastValueSection;
-    if (points.length > 0) {
-      const last = points[points.length - 1];
-      const lastDate = formatDate(last.date);
-      const lastValue = last.value.length > 1 ? last.value.join('/') : last.value;
-      lastValueSection = (
-        <span className="measurement__lastValue">
-          <div>{lastDate}</div>
-          <div className="measurement__lastValue__value">{`${lastValue} ${last.unit}`}</div>
-        </span>
-      );
-    }
-
     return (
       <div className="measurement" >
         <span className="measurement__name">{name}</span>
         <span className="measurement__chart">
           {chart}
         </span>
-        {lastValueSection}
       </div>
     );
   }
