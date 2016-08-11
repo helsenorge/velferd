fab8513707f7ac63ce0524ad898d54795eceda9e
carlodiaz <carlo.diaz@ehelse.no>
2016-08-11 11:11:15 +0000

    Display columns for for all days on questionnaire responses table

--- src/components/questionnaire-responses-container/questionnaire-responses/questionnaire-responses.jsx
+++ src/components/questionnaire-responses-container/questionnaire-responses/questionnaire-responses.jsx
@@ -6,35 +6,53 @@ class QuestionnaireResponses extends Component {
 
   getQuestions(entries) {
     const questions = {};
+
     for (let i = 0; i < entries.length; i++) {
       const entry = entries[i];
 
       for (let ii = 0; ii < entry.resource.group.group[0].question.length; ii++) {
-        const question = entry.resource.group.group[0].question[i];
+        const question = entry.resource.group.group[0].question[ii];
+
         if (!questions[question.linkId]) {
-          questions[question.linkId] = { text: question.text, answers: [] };
+          questions[question.linkId] = { text: question.text, answers: {} };
         }
-        questions[question.linkId].answers.push(question.answer[0].valueCoding.code);
+        const date = new Date(entry.resource.authored).toLocaleDateString();
+        const value = question.answer[0].valueCoding.code;
+        questions[question.linkId].answers[date] = value;
       }
     }
     return questions;
   }
 
-  render() {
-    const entries = filterEntries(
-      this.props.data.entry, this.props.fromDate, this.props.toDate);
-    const questions = this.getQuestions(entries);
+  getRows(questions, fromDate, toDate) {
     const rows = [];
-
     Object.keys(questions).forEach((key) => {
       if (questions.hasOwnProperty(key)) {
         const question = questions[key];
-        const cols = question.answers.map((answer) => (
-          <td>{answer}</td>
-        ));
-        rows.push(<tr key={key}><td>{question.text}</td>{cols}</tr>);
+        const cells = this.getCells(question, fromDate, toDate);
+        rows.push(<tr key={key}><td>{question.text}</td>{cells}</tr>);
       }
     });
+    return rows;
+  }
+
+  getCells(question, fromDate, toDate) {
+    const cells = [];
+    for (let d = new Date(fromDate);
+          d.getTime() < toDate.getTime();
+          d.setDate(d.getDate() + 1)) {
+      const date = d.toLocaleDateString();
+      const value = question.answers[date];
+      cells.push(<td>{value}</td>);
+    }
+    return cells;
+  }
+
+  render() {
+    const { data, fromDate, toDate } = this.props;
+    const entries = filterEntries(data.entry, fromDate, toDate);
+    const questions = this.getQuestions(entries);
+    const rows = this.getRows(questions, fromDate, toDate);
 
     return (
       <div className="questionnaire" >
