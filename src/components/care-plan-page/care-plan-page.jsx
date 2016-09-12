c6d2470d444619d2f4189ede18f440f5a868dfdf
Sjur Seibt <sjurlur@gmail.com>
2016-09-12 16:36:41 +0200

    header text

--- src/components/care-plan-page/care-plan-page.jsx
+++ src/components/care-plan-page/care-plan-page.jsx
@@ -34,6 +34,7 @@ class CarePlanPage extends Component {
     const isEmpty = phases.length === 0;
     return (
       <div className="care-plan-page">
+        <h2 className="care-plan-page__heading">Egenbehandlingsplan</h2>
         {isEmpty
           ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
           : <div style={{ opacity: isFetching ? 0.5 : 1 }}>
