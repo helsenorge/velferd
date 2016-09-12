8dbfa250f5aacc94dc6bf311296cb2cdf129a7a7
Sjur Seibt <sjurlur@gmail.com>
2016-09-12 16:55:32 +0200

    legg til overordnet mål

--- src/components/dashboard-page/header/header.jsx
+++ src/components/dashboard-page/header/header.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ZoomButtons from './zoombuttons/zoombuttons.jsx';
 import Range from './range/range.jsx';
 import Report from './report/report.jsx';
+import Goal from './goal/goal.jsx';
 import './header.scss';
 
 const Header = (props) => (
   <header className="dashboard-header">
     <h2 className="dashboard-header__heading">Egenvurdering og målinger</h2>
+    <Goal />
     <div className="dashboard-header__flexwrap">
       <Report {...props} />
       <ZoomButtons {...props} />
