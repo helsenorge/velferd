e0b7e18c979056dabefb305e5b113b162914b6f3
Sjur Seibt <sjurlur@gmail.com>
2016-09-27 10:49:20 +0200

    flytt controls css til egen fil

--- src/components/care-plan-page/controls/controls.jsx
+++ src/components/care-plan-page/controls/controls.jsx
@@ -4,43 +4,44 @@ import Spinner from '../../spinner/spinner.jsx';
 import Icon from '../../icon/icon.jsx';
 import iconPrint from '../../../../svg/print.svg';
 import iconEdit from '../../../../svg/edit.svg';
+import './controls.scss';
 
 const Controls = ({ edit, editCarePlan, saveCarePlan, saving, cancel }) => {
   const cardClasses = classNames({
-    'care-plan-page__card': true,
-    'care-plan-page__card--flipped': edit,
+    controls__card: true,
+    'controls__card--flipped': edit,
   });
   return (
-    <div className="care-plan-page__controls">
+    <div className="controls">
       <div className={cardClasses}>
-        <div className="care-plan-page__front">
+        <div className="controls__front">
           <button
             onClick={editCarePlan}
-            className="care-plan-page__button care-plan-page__button--print"
+            className="controls__button controls__button--print"
           >
             <Icon glyph={iconPrint} />
             Skriv ut egenbehandlingsplan
           </button>
           <button
             onClick={editCarePlan}
-            className="care-plan-page__button care-plan-page__button--edit"
+            className="controls__button controls__button--edit"
           >
             <Icon glyph={iconEdit} />
             Rediger
           </button>
         </div>
-        <div className="care-plan-page__back">
+        <div className="controls__back">
           {saving ? (<Spinner />) : null}
           <button
             onClick={saveCarePlan}
-            className="care-plan-page__button care-plan-page__button--save"
+            className="controls__button controls__button--save"
             disabled={saving}
           >
             Lagre
           </button>
           <button
             onClick={cancel}
-            className="care-plan-page__button care-plan-page__button--cancel"
+            className="controls__button controls__button--cancel"
             disabled={saving}
           >
             Avbryt
