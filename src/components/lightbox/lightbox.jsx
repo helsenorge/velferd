7cd3ffcd958dfb84e248747834d9ee2fa4a1d517
Sjur Seibt <sjurlur@gmail.com>
2016-09-29 15:15:54 +0200

    prevent scroll lightbox

--- src/components/lightbox/lightbox.jsx
+++ src/components/lightbox/lightbox.jsx
@@ -1,21 +1,33 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import './lightbox.scss';
 import Icon from '../icon/icon.jsx';
 import iconPlus from '../../../svg/plus.svg';
 
-const Lightbox = ({ onClose, children, className = '' }) => (
-  <div className="lightbox">
-    <div className="lightbox__container">
-      <button onClick={onClose} className="lightbox__close-button">
-        <Icon glyph={iconPlus} className="lightbox__close-button-icon" />
-        <span className="lightbox__close-button-text">Lukk</span>
-      </button>
-      <div className={className}>
-        {children}
+class Lightbox extends Component {
+  componentDidMount() {
+    document.body.classList.add('noscroll');
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('noscroll');
+  }
+
+  render() {
+    return (
+      <div className="lightbox">
+        <div className="lightbox__container">
+          <button onClick={this.props.onClose} className="lightbox__close-button">
+            <Icon glyph={iconPlus} className="lightbox__close-button-icon" />
+            <span className="lightbox__close-button-text">Lukk</span>
+          </button>
+          <div className={this.propsclassName}>
+            {this.props.children}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  );
+      );
+  }
+}
 
 Lightbox.propTypes = {
   onClose: PropTypes.func.isRequired,
