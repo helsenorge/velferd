b902fd2898a16a808edef9ae6aa4ab06becffa90
Sjur Seibt <sjurlur@gmail.com>
2016-09-29 14:03:27 +0200

    expander og css history

--- src/components/care-plan-page/history-container/history/history.jsx
+++ src/components/care-plan-page/history-container/history/history.jsx
@@ -1,6 +1,16 @@
 import React, { Component, PropTypes } from 'react';
-
+import Collapse from 'react-collapse';
+import classNames from 'classnames';
+import iconChevron from '../../../../../svg/chevron.svg';
+import Icon from '../../../icon/icon.jsx';
+import './history.scss';
 class History extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isOpened: false };
+    this.toggleOpened = this.toggleOpened.bind(this);
+  }
 
   getRows(versions) {
     const rows = [];
@@ -9,25 +19,52 @@ class History extends Component {
       const date = new Date(version.date).toLocaleDateString();
       rows.push(
         <tr key={i}>
-          <td>{date}</td>
-          <td>{name}</td>
-          <td>{version.comment}</td>
+          <td className="historytable__cell historytable__cell--date">{date}</td>
+          <td className="historytable__cell">{name}</td>
+          <td className="historytable__cell">{version.comment}</td>
         </tr>);
     });
 
     return rows;
   }
 
+  toggleOpened() {
+    this.setState({ isOpened: !this.state.isOpened });
+  }
+
   render() {
     const { versions } = this.props;
+    const { isOpened } = this.state;
     const rows = this.getRows(versions);
-
+    const iconClasses = classNames({
+      historytable__expandericon: true,
+      'historytable__expandericon--open': isOpened,
+    });
     return (
-      <table className="questionnaire-responses-table">
-        <tbody>
-        {rows}
-        </tbody>
-      </table>
+      <div className="historytable">
+        <button
+          className="historytable__expanderbutton"
+          aria-controls="collapse"
+          onClick={this.toggleOpened}
+        >
+          Tidligere versjoner
+          <Icon glyph={iconChevron} className={iconClasses} />
+        </button>
+        <Collapse id="collapse" isOpened={isOpened} aria-expanded={isOpened}>
+          <table className="historytable__table">
+            <thead>
+              <tr>
+                <th className="historytable__header">Sist oppdatert</th>
+                <th className="historytable__header">Endret av</th>
+                <th className="historytable__header">Kommentar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+        </Collapse>
+      </div>
     );
   }
 }
