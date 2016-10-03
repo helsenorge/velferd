c6ebce7c4293529274403aa6e9952fef4b1d0bb6
Sjur Seibt <sjurlur@gmail.com>
2016-10-03 11:54:45 +0200

    pagemargin

--- src/components/pageheader/pageheader.jsx
+++ src/components/pageheader/pageheader.jsx
@@ -3,12 +3,14 @@ import './pageheader.scss';
 
 const PageHeader = () => (
   <header className="pageheader">
-    <span>
-      Tora Hansen 1230399 12345 hjertesvikt
-    </span>
-    <span>
-      Innlogget som: Anna Fos Eieb (lege), Lillesand legekontor
-    </span>
+    <div className="pageheader__wrapper">
+      <span>
+        Tora Hansen 1230399 12345 hjertesvikt
+      </span>
+      <span>
+        Innlogget som: Anna Fos Eieb (lege), Lillesand legekontor
+      </span>
+    </div>
   </header>
 );
 
