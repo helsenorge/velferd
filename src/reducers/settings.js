e3bbed20d1caabac4cbf6d7abce70c0418106ba6
Sjur Seibt <sjurlur@gmail.com>
2016-10-13 15:48:25 +0200

    Revert "changed patientId"
    This reverts commit d7932b363a2c12712f226d9ac27e5734e445fb02.

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -6,7 +6,7 @@ import {
 export function settings(state = {
   authenticate: false,
   fhirUrl: 'http://continua.cloudapp.net/baseDstu2',
-  patientId: '72325',
+  patientId: '1-26',
   questionnaireId: '62763',
 }, action) {
   switch (action.type) {
