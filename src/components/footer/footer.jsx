2f14c41e518996359c9ba3861dc44adcc389772f
Sjur Seibt <sjurlur@gmail.com>
2016-10-28 09:39:31 +0200

    legg til footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,13 @@
 import React, { PropTypes } from 'react';
+import Icon from '../icon/icon.jsx';
+import ehelselogo from '../../../svg/Direktoratet_for_e-helse_horisontal_logo_RGB_sort.svg';
+import './footer.scss';
 
 const Footer = function Footer(props) {
   const api = `API: ${props.fhirUrl}`;
   return (
-    <footer>
+    <footer className="footer">
+      Tjenesten er levert av: <Icon glyph={ehelselogo} className="footer__icon" /><br />
       <small>{api}</small>
     </footer>
   );
