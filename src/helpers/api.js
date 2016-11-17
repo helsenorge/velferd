45880b0e3bfd96f99d37c53029cda88e97f6143a
carlodiaz <carlo.diaz@ehelse.no>
2016-11-17 12:28:24 +0000

    Set Accept json header

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,22 +1,24 @@
 import 'whatwg-fetch';
 
 export function get(url, token, useXAuthTokenHeader) {
-  let init;
+  const headers = new Headers();
+  headers.append('Accept', 'application/json+fhir');
 
   if (token) {
-    init = useXAuthTokenHeader ? {
-      method: 'get',
-      mode: 'cors',
-      credentials: 'include',
-      headers: { 'X-Auth-Token': `${token}` },
+    if (useXAuthTokenHeader) {
+      headers.append('X-Auth-Token', token);
     }
-    : {
-      method: 'get',
-      mode: 'cors',
-      credentials: 'include',
-      headers: { Authorization: `Bearer ${token}` },
-    };
+    else {
+      headers.append('Authorization', `Bearer ${token}`);
+    }
   }
+
+  const init = {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+    headers,
+  };
 
   return fetch(url, init);
 }
@@ -57,6 +59,8 @@ export function post(url, data, token, useXAuthTokenHeader) {
       headers.append('Authorization', `Bearer ${token}`);
     }
   }
+
+  headers.append('Content-Type', 'application/json');
 
   const init = {
     method: 'POST',
